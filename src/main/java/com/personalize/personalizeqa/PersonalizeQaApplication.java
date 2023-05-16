2190f1b1909062ddc0f39dedb0dd4727c91f59d8
zhengyao <zhengyao94@hotmail.com>
2023-05-16 20:16:19 +0800

    第一次提交

---
+++ src/main/java/com/personalize/personalizeqa/PersonalizeQaApplication.java
@@ -0,0 +1,13 @@
+package com.personalize.personalizeqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizeQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizeQaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalize/personalizeqa/PersonalizeQaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalize.personalizeqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizeQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
